e6d779939a2e2fcc4e3736158ac0621e0c6d5b8b
Lucas Vinicius <lucasviniciussm1@icloud.com>
2024-08-07 10:57:39 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,63 @@
+import { ChangeDetectorRef, Component, ElementRef, importProvidersFrom, QueryList, ViewChildren } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import { CommonModule } from '@angular/common';
+import {CdkDragDrop, CdkDrag, moveItemInArray, DragDropModule} from '@angular/cdk/drag-drop';
+import { HttpClient, HttpClientModule, HttpHeaders, provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
+import { Steps } from './enums';
+import {ClipboardModule} from '@angular/cdk/clipboard'
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+	RouterOutlet,
+	MatFormFieldModule,
+	FormsModule,
+	MatButtonModule, 
+	MatIconModule,
+	MatInputModule,
+	MatIconModule,
+	ReactiveFormsModule,
+	MatCheckboxModule,
+	CommonModule,
+	DragDropModule,
+	ClipboardModule,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+})
+export class AppComponent {
+	formGroup: FormGroup;
+	result: string = '';
+	step: Steps = Steps.Init;
+
+	constructor(private fb: FormBuilder, private http: HttpClient) {
+		this.formGroup = fb.group({
+			text: this.fb.control('', Validators.required)
+		})
+	}
+	
+	onSubmit(){
+		this.http.post<Result>('/api', {url: this.formGroup.value.text}).subscribe({
+			next: (r) => {
+				this.result = r.urlEncurtada;
+				this.step = Steps.Created;
+			}
+		})
+	}
+
+	back() {
+		this.formGroup.get('text')?.reset();
+		this.step = Steps.Init;
+	}
+}
+
+export interface Result {
+	urlEncurtada: string
+}
---
+++ src/app/enums.ts
@@ -0,0 +1,4 @@
+export enum Steps {
+	Init = "Init",
+	Created = "Created",
+}
